927378f01e2f581203b9aa835097a817e172ccf3
tintedsky <hongbonew@gmail.com>
2017-10-13 13:50:34 -0400

    String manipulation in Objective C 1. The equal sign just compare the address. If two string pointers are assigned to the same literals, they point to the same address. 2. Using NSString function to compare, reorganize and concatenate strings in Objective C; 3. Read document on NSString to find help.

--- StringFling/ViewController.m
+++ StringFling/ViewController.m
@@ -17,6 +17,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *firstName = @"Mcqueen";
+    
+    //NSString *allocatedString = [[NSString alloc]init];
+    //Most Most of time the pointer is initialized as Literals, not like this.
+    NSLog(@"Firstname: %@", firstName);
+    
+    //stringWithFormat
+    //stringWithFormat is a static function, marked as '+'
+    NSString *fullName = [NSString stringWithFormat:@"fullName: %@ %@", firstName, @"Muge"];
+    NSLog(@"FullName: %@", fullName);
+    
+    //In swift, let fullName = "\(firstName) \(lastName)"
+    //String concatenation.
+    
+    //stringByAppendingString is not a static function, marked with '-'
+    NSString *display = [fullName stringByAppendingString:@" Niu"];
+    NSLog(@"Complete full name: %@", display);
+    
+    //String comparason
+    NSString *var1 = @"Junk";
+    NSString *var2 = @"Junk";
+    NSString *var3 = @"jUnk";
+    
+    //Here compare the address instead of the content of String
+    if (var1 == var2) {
+        NSLog(@"They are the same! using ==");
+        //NSLog(@"var1 address: %lu, var2 address: %lu", var1, var2);
+    }
+    
+    if ([var1.lowercaseString isEqualToString:var3.lowercaseString]){
+        NSLog(@"They are the same using isEqualToString function.");
+    }
+    
+    //caseInsensitiveCompare does not return Yes or No, it returns an enum as below:
+    //typedef NS_ENUM(NSInteger, NSComparisonResult) {NSOrderedAscending = -1L, NSOrderedSame, NSOrderedDescending};
+    if ([var1 caseInsensitiveCompare:var3] == NSOrderedSame){
+        NSLog(@"Var1 is the same as var3 if we ignore the case");
+    }
+
 }
 
 
